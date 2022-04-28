701862aa6ebc2159a98a2f95bf70150d7b5c1ea4
unfound <448217185@qq.com>
2022-04-28 21:30:18 +0800

    leetcode: 无重复字符最长子串

---
+++ leetcode/bytedance.无重复字符的最长子串.ts
@@ -0,0 +1,48 @@
+function lengthOfLongestSubstringOld(s: string): number {
+    if (s.length <= 0) return 0
+    let maxLength = 1
+    let start = 0, end = 1
+    while (end < s.length) {
+        let isSame = false
+        let sameStart = start
+        for (let i = start; i < end; i++) {
+            if (s[end] === s[i]) {
+                isSame = true
+                sameStart = i
+                break
+            }
+        }
+        // console.log(start, end)
+        if (isSame) {
+            // console.log(start, end)
+            maxLength = Math.max(maxLength, end - start)
+            start = sameStart + 1
+        } else if (end === s.length - 1) {
+            maxLength = Math.max(maxLength, end - start + 1)
+        }
+    
+        end++
+    }
+
+    return maxLength
+};
+
+function lengthOfLongestSubstring(s: string): number {
+    let maxLength = 0, start = 0
+    let map = new Map<string, number>()
+    for (let end = 0; end < s.length; end++) {
+        const char = s.charAt(end)
+        if (map.has(char)) {
+            start = Math.max(start, map.get(char)! + 1)
+        }
+        maxLength = Math.max(maxLength, end - start + 1)
+        map.set(char, end)
+    }
+    return maxLength
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
+console.log(lengthOfLongestSubstring('bbbbb'))
+console.log(lengthOfLongestSubstring('pwwkew'))
+console.log(lengthOfLongestSubstring('dvdf'))
+console.log(lengthOfLongestSubstring('au'))
